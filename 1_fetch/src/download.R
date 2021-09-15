da24d1987e6e8f25cb2d3fa887b9b31b60665710
Andy McAliley <wmcaliley@usgs.gov>
2021-09-15 11:48:06 -0600

    Change download_data arg from dir to filepath

--- 1_fetch/src/download.R
+++ 1_fetch/src/download.R
@@ -1,8 +1,8 @@
 library(sbtools)
 
-download_data <- function(output_dir = file.path('1_fetch', 'out')) {
-  dir.create(output_dir, showWarnings = FALSE)
+download_data <- function(out_filepath = file.path('1_fetch', 'out', 'model_RMSEs.csv')) {
+  dir.create(dirname(out_filepath), showWarnings = FALSE)
   # Get the data from ScienceBase
-  mendota_file <- file.path(output_dir, 'model_RMSEs.csv')
-  item_file_download('5d925066e4b0c4f70d0d0599', names = 'me_RMSE.csv', destinations = mendota_file, overwrite_file = TRUE)
+  item_file_download('5d925066e4b0c4f70d0d0599', names = 'me_RMSE.csv',
+                     destinations = out_filepath, overwrite_file = TRUE)
 }
